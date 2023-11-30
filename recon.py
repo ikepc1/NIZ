9eaefe9e5a6f801bde2a6891dd88647e9fcee11d
Isaac Buckland <ikepc1@gmail.com>
2023-11-30 14:43:09 -0700

    hh

--- recon.py
+++ recon.py
@@ -6,18 +6,6 @@ from typing import Callable
 from tyro_fit import tyro
 from niche_plane import get_event_from_df, NichePlane, NicheFit
 
-# def run_multiprocessing(func: Callable[[object],object], inputs: list[object]) -> list[object]:
-#     '''This function maps a function to imap with the use of context managers
-#     and a progress bar.
-#     '''
-#     results = []
-#     with alive_bar(len(inputs)) as bar:
-#         with Pool(cpu_count()) as p:
-#             for result in p.imap(func, inputs, chunksize=250):
-#                 results.append(result)
-#                 bar()
-#     return results
-
 def run_multiprocessing(func: Callable[[object],object], inputs: list[object], chunksize = 250) -> list[object]:
     '''This function maps a function to imap with the use of context managers
     and a progress bar.
