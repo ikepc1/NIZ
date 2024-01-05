5ecb9b9641a3a76cc08d848fe6ed93b5f72186ab
Isaac Buckland <ikepc1@gmail.com>
2024-01-04 17:29:32 -0700

    works but overestimates

--- recon.py
+++ recon.py
@@ -53,12 +53,12 @@ def base_fit_params() -> pd.DataFrame:
     minuit.
     '''
     l = []
-    l.append({'name':'Xmax', 'initial_value': 500., 'limits':(1.e2,1.e3), 'fixed': False})
+    l.append({'name':'Xmax', 'initial_value': 500., 'limits':(3.e2,1.e3), 'fixed': False})
     l.append({'name':'Nmax', 'initial_value': 1.e7, 'limits':(1.e4,1.e8), 'fixed': False})
     l.append({'name':'zenith', 'initial_value': 0., 'limits':(0.,np.pi/2), 'fixed': False})
     l.append({'name':'azimuth', 'initial_value': 0., 'limits':(0.,2*np.pi), 'fixed': False})
-    l.append({'name':'corex', 'initial_value': 0., 'limits':(COUNTER_POSITIONS[:,0].min(), COUNTER_POSITIONS[:,0].max()), 'fixed': False})
-    l.append({'name':'corey', 'initial_value': 0., 'limits':(COUNTER_POSITIONS[:,1].min(), COUNTER_POSITIONS[:,1].max()), 'fixed': False})
+    l.append({'name':'corex', 'initial_value': 0., 'limits':(COUNTER_POSITIONS[:,0].min(), COUNTER_POSITIONS[:,0].max()), 'fixed': True})
+    l.append({'name':'corey', 'initial_value': 0., 'limits':(COUNTER_POSITIONS[:,1].min(), COUNTER_POSITIONS[:,1].max()), 'fixed': True})
     l.append({'name':'corez', 'initial_value': 0., 'limits':(COUNTER_POSITIONS[:,2].min(), COUNTER_POSITIONS[:,2].max()), 'fixed': True})
     l.append({'name':'X0', 'initial_value': 0., 'limits':(-500.,500.), 'fixed': True})
     l.append({'name':'Lambda', 'initial_value': 70., 'limits':(0.,100.), 'fixed': True})
@@ -66,7 +66,7 @@ def base_fit_params() -> pd.DataFrame:
     df = df.set_index('name')
     return df
 
-def construct_times(trigtime: float) -> np.ndarray:
+def full_wf_times(trigtime: float) -> np.ndarray:
     ''''''
     times_ns = np.arange(NICHE_TIMEBIN_SIZE*WAVEFORM_SIZE, step = NICHE_TIMEBIN_SIZE)
     times_ns -= times_ns[TRIGGER_POSITION]
@@ -86,16 +86,18 @@ class EventFit:
     plane_fit: NichePlane
     tyro: TyroFit
     pe: ProcessEvents
-    waveform_cushion: int = 50
+    waveform_cushion: int = 10
 
     def __post_init__(self) -> None:
         self.core = self.tyro.core_estimate
         self.real_wf_array = np.array([f.waveform[TRIGGER_POSITION-self.waveform_cushion:TRIGGER_POSITION+self.waveform_cushion] for f in self.plane_fit.counters]).flatten()
         self.real_nfit_dict = {f.name:f for f in self.plane_fit.counters}
         self.real_trigger_ids = np.array([COUNTER_NO[f.name] for f in self.plane_fit.counters])
-        self.real_trigger_names = np.array([f.name for f in self.plane_fit.counters])
+        self.real_trigger_names = np.array([f.name for f in self.plane_fit.counters], dtype=str)
         self.real_wf_error = np.sqrt(self.real_wf_array)
+        self.trigtimes = get_times_array(self.plane_fit.counters)
         self.real_wf_times = self.construct_times()
+        self.first_trigger_name = self.real_trigger_names[np.argmin(self.trigtimes)]
         if self.plane_fit.phi < 0.:
             self.plane_fit.phi += 2*np.pi
         self.params = self.init_fit_params()
@@ -109,8 +111,7 @@ class EventFit:
 
     def construct_times(self) -> np.ndarray:
         ''''''
-        trigtimes = get_times_array(self.plane_fit.counters)
-        times_array = np.array([self.get_waveform_times(time) for time in trigtimes])
+        times_array = np.array([self.get_waveform_times(time) for time in self.trigtimes])
         return times_array.flatten()
 
     def init_fit_params(self) -> pd.DataFrame:
@@ -141,37 +142,47 @@ class EventFit:
         Lambda=parameters[8]
     )
 
-    def get_trace_portion()
+    def get_times_dict(self, sim_nfits: dict[str,NicheFit]) -> dict[str,np.ndarray]:
+        '''Have the zero time be when the first real counter triggered, return a dictionary
+        of the time arrays.
+        '''
+        trigtimes = np.array([sim_nfits[name].trigtime() for name in sim_nfits])
+        trigtimes = np.float64(trigtimes - sim_nfits[self.first_trigger_name].trigtime())
+        return {name:full_wf_times(time) for name, time in zip(sim_nfits, trigtimes)}
 
     def model(self, times: np.ndarray, parameters: np.ndarray) -> np.ndarray:
         ev = self.get_event(parameters)
         sim_nfits = self.pe.gen_nfits_from_event(ev)
         sim_nfit_dict = {f.name:f for f in sim_nfits}
         traces_at_times = np.zeros((len(self.real_trigger_ids),2*self.waveform_cushion))
+
+        #if the earliest trigger didn't occur, return zeroes b/c otherwise times can't be matched
+        if self.first_trigger_name not in sim_nfit_dict:
+            return traces_at_times.flatten()
+        
+        #if only one counter triggers nothing can be done
+        if len(sim_nfits) == 1:
+            return traces_at_times.flatten()
+
+        #otherwise the times can have the same zero, get the times for each waveform entry in each counter
+        sim_times_dict = self.get_times_dict(sim_nfit_dict)
+
         for i,name in enumerate(self.real_trigger_names):
             if name in sim_nfit_dict:
-                
-                traces_at_times[i] = 
+                starti = i*2*self.waveform_cushion
+                ctimes = times[starti:starti+2*self.waveform_cushion]
+
+                #find trace values in simulated waveforms at the times the real triggers occurred
+                if ctimes.max() in sim_times_dict[name]:
+                    traces_at_times[i] = sim_nfit_dict[name].waveform[np.searchsorted(sim_times_dict[name], ctimes)]
+        return traces_at_times.flatten()
         
-
-    def model(self, det_ids: np.ndarray, parameters: np.ndarray) -> np.ndarray:
-        '''This is the fit model to be passed to iminuit.
-        '''
-        ev = self.get_event(parameters)
-        sim_nfits = self.pe.gen_nfits_from_event(ev)
-        sim_nfit_dict = {f.name:f for f in sim_nfits}
-        sim_pa_array = np.zeros_like(det_ids)
-        for i,id in enumerate(det_ids):
-            if NAMES[id] in sim_nfit_dict:
-                sim_pa_array[i] = sim_nfit_dict[NAMES[id]].intsignal
-        return sim_pa_array
-
     def fit(self) -> Minuit:
         names = tuple(self.params.index)
         values = tuple(self.params['initial_value'])
         fixed = tuple(self.params['fixed'])
         limits = tuple(self.params['limits'])
-        least_squares_np = LeastSquares(self.real_trigger_ids, self.data_pa_array, self.real_pa_error, self.model)
+        least_squares_np = LeastSquares(self.real_wf_times, self.real_wf_array, self.real_wf_error, self.model)
         m = Minuit(least_squares_np, values, name=names)
         for name, fix, lim in zip(names, fixed, limits):
             m.fixed[name] = fix
@@ -189,7 +200,7 @@ if __name__ == '__main__':
     g = plot_generator(ns)
 
     pe = ProcessEvents(cfg, frozen_noise=True)
-    ev = Event(0.,500,5.e6,np.deg2rad(40.),np.deg2rad(315.), 450., -660.,-25.,0,70)
+    ev = Event(0.,400,1.e6,np.deg2rad(40.),np.deg2rad(315.), 450., -660.,-25.,0,70)
     sim_nfits = ProcessEvents(cfg, frozen_noise=True).gen_nfits_from_event(ev)
     pf = NichePlane(sim_nfits)
     ty = tyro(sim_nfits)
@@ -197,6 +208,7 @@ if __name__ == '__main__':
 
     # ev_id = 6
     # ef = EventFit(ns.loc[ev_id,'Plane Fit'], ns.loc[ev_id,'Fit'],ProcessEvents(cfg, frozen_noise=True))
+    print('starting gradient descent...')
     migrad=ef.fit().migrad()
     pars = np.array([par.value for par in migrad.params])
     sim_ev = ef.get_event(pars)
